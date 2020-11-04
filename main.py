c570163125ec67ebdbe0b327ef4f518cfd0b9584
Oliver Lockwood (CBRE) <v-ollock@microsoft.com>
2020-11-04 11:40:52 -0800

    v1

--- main.py
+++ main.py
@@ -16,13 +16,32 @@ def get_fact():
 
     return facts[0].getText()
 
+def pig_it(pig):
+    payload = dict(input_text=pig)
+    r = requests.post('http://hidden-journey-62459.herokuapp.com/piglatinize/', data=payload)
+    soup = BeautifulSoup(r.content, "html.parser")
+    facts = soup.text
+    factstring = str(facts.rsplit('Esultray')[1]).strip()
+    return factstring
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    pigged = pig_it(fact)
+    return pigged
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
 
+'''
+Use request library to send 
+form data is a key value pair
+
+General
+    Request URL: http://hidden-journey-62459.herokuapp.com/piglatinize/
+    Request Method: POST
+Form Data
+    input_text: I am sending
+    '''
